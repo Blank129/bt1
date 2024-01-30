6e89136e7421d8aacd2c131c82ef4825b3c375ed
dung <dungq2003@gmail.com>
2024-01-30 19:49:36 +0700

    first commit

---
+++ btWeb/btWeb/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using btWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace btWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        List<ProductModel> list = new List<ProductModel>
+        {
+            new ProductModel{ Id =  1, Name ="Quan", Price = 10},
+            new ProductModel{ Id =  2, Name ="Ao", Price = 10}
+        };
+        public ActionResult Index()
+        {
+            return View(list);
+        }
+
+        public PartialViewResult getProduct()
+        {
+            return PartialView("getProduct", list);
+        }
+
+        [HttpPost]
+        public JsonResult AddProduct(ProductModel newProduct)
+        {
+            newProduct.Id = list.Count + 1;
+            list.Add(newProduct);
+            return Json(new {success = true, Product = newProduct});
+        }
+    }
+}
---
+++ btWeb/btWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="btWeb.MvcApplication" Language="C#" %>
